d0062f43fec907c815395fe466adda3727829092
superwonso <67905961+superwonso@users.noreply.github.com>
2020-07-25 16:41:04 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (lenght int, uppercase string) {
+	defer fmt.Println("I'm done")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func main() {
+	totalLenght, up := lenAndUpper("nico")
+	fmt.Println(totalLenght, up)
+	fmt.Println("Hello!")
+}
+
+//help!
