162a5582cfd51c126c5044e267a4a0cfd593af0c
superwonso <chadonghyeon@kakao.com>
2020-07-31 23:12:50 +0900

    20200731

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var a,b,c,n,i int
-	fmt.Scanf("%d %d %d", &a, &b , &n)
-	c = a/b
-	for (i:=0; (c%10)!=0; i+){
-		c=c*10
-		if (n<i){
-			break
-		}
-	}
-fmt.Println(i%10)
+	var a, b, c float64
+	var count, result, n, i, temp int
+	fmt.Scanf("%f %f %f", &a, &b, &n)
+	c = a / b
+	float64(temp)
+	math.pow(10, n) = temp
+	float64(i)
+	i = c * temp
+	int(i)
+	fmt.Println(i % 10)
 }
+
+//fck https://www.acmicpc.net/problem/1312
